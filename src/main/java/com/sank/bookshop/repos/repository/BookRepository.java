ca2a76f9694c153ac57c0241ed5bca0cef80ae5c
Sankar <sankar_pro@rediffmail.com>
2023-05-14 19:13:24 +0200

    Triumph:DAO - Read empty json file respond empty

--- src/main/java/com/sank/bookshop/repos/repository/BookRepository.java
+++ src/main/java/com/sank/bookshop/repos/repository/BookRepository.java
@@ -1,10 +1,28 @@
 package com.sank.bookshop.repos.repository;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-
+@Component
 public class BookRepository {
-    public List<String> findAll() {
+    @Autowired
+    private ResourceLoader resourceLoader;
+    public List<String> findAll()  {
+        ObjectMapper mapper = new ObjectMapper();
+        Resource resource = resourceLoader.getResource("classpath:masterbookfeed.json");
+        try (FileReader reader = new FileReader(resource.getFile())) {
+            return mapper.readValue(reader, new TypeReference<List<String>>() {
+            });
+        } catch (IOException e) {
+        }
         return Collections.emptyList();
     }
 }
--- src/test/java/com/sank/bookshop/repos/repository/BookRepositoryTest.java
+++ src/test/java/com/sank/bookshop/repos/repository/BookRepositoryTest.java
@@ -3,13 +3,42 @@ package com.sank.bookshop.repos.repository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BookRepositoryTest {
+    @Mock
+    Resource resource;
+    @Mock
+    private ResourceLoader resourceLoader;
+    @InjectMocks
+    private BookRepository bookRepository;
+
     @Test
-    public void testAlwaysReturnsNonNull() {
-        BookRepository bookRepository = new BookRepository();
+    public void testAlwaysReturnsNonNull() throws IOException {
+        when(resourceLoader.getResource(Mockito.anyString())).thenReturn(resource);
+        when(resource.getFile()).thenReturn(new File("test"));
         Assert.assertNotNull(bookRepository.findAll());
+    }
+
+    @Test
+    public void testReadAJsonFile() throws IOException, URISyntaxException {
+        when(resourceLoader.getResource(Mockito.anyString())).thenReturn(resource);
+        when(resource.getFile()).thenReturn(new File(this.getClass()
+                                                         .getResource("/masterbookfeed.json")
+                                                         .toURI()));
+        Assert.assertTrue(bookRepository.findAll()
+                                        .isEmpty());
     }
 }
